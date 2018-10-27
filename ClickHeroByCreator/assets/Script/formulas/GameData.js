ed423911d4bc007909f0e753c431bf2b8bc08300
CoreXJ <CoreXJ@Mac.local>
2018-10-27 17:28:51 +0800

    古神的影响 和 技能的影响

--- ClickHeroByCreator/assets/Script/formulas/GameData.js
+++ ClickHeroByCreator/assets/Script/formulas/GameData.js
@@ -20,21 +20,40 @@ cc.Class({
         DPSClickDamage : 0,
         globalDPSTimes : 1,
         globalGoldTimes : 1,
-        clickCrit : 1,// 暴击倍数
-        clickCritOdds : 0,// 暴击概率
+        critTimes : 1,// 暴击倍数
+        critOdds : 0,// 暴击概率
 
         //--------古神的影响--------
         addGoldenDpsTimes : 0,// 第一个古神 会影响数值 0.02++
         addPrimalBossOdds : 0,// 增加远古Boss出现几率
         addPowersurgeSecond : 0,// Powersurge秒数增加 2s++
-        addBjTimes : 0,//古神附加暴击倍数
+        addCritTimes : 0,//古神附加暴击倍数
         addClickstormSecond : 0, //点击风暴秒数增加 2s++
         addBossTimerSecond : 0,// Boss计时器持续时间
-        // add
+        addTreasureOdds : 0.01,// 宝箱出现概率
+        addMetalDetectorSecond : 0,// 金币探测器 2s++
+        addTenfoldGoldOdds : 0, // 普怪 宝箱 10倍金币的概率
+        addClickDamageTimes : 1,// 点击伤害倍数 每级+20%
+        addSuperClickSecond : 0,// 超级点击时间 2s++
+        addDPSClickDamageTimes : 0,// 附加DPS点击伤害倍数
+        addGoldClickSecond : 0,//点金手时间 2s++
+        addLeaveGoldTimes : 1,// 闲置金币倍数
+        addGoldTimes : 1,//+5% Gold
+        addTreasureTimes : 1,//宝箱金币倍数
+        addGoldClickTimes : 1,// 点金手倍数 +30% gold from Golden Clicks 
+        addLeaveDPSTimes : 1,//加闲置DPS伤害
+        addCritStormSecond : 0,//增加暴击风暴时间 +2s
+        addSkillCoolReduction : 0,//技能冷却减少
 
-        //--------技能的影响--------
-        powersurgeTimes : 1,//能量风暴DPS倍数 触发技能时 改为对应倍数，技能结束要改回为1
-
+        //--------被动技能的影响--------
+        cskCritTimes : 1, // 暴击倍数 :calCritTimes()
+        cskCritOdds : 0, // 附加暴击概率 :calCritOdds()
+        //--------主动技能的影响--------
+        powersurgeTimes : 1,//能量风暴DPS倍数 触发技能时 改为对应倍数，技能结束要改回为1 :refresh()
+        skCritOdds : 0,// 当开启暴击风暴时 设置为0.5 结束改回0 :calCritOdds()
+        skGoldTimes : 1,// 当开启金币探测器时 改为2 结束为1 :calGoldTimes()
+        skDPSTimes : 1,// 当使用一次黑暗仪式，乘以1.05 :refresh()
+        skClickTimes : 1,// 开启超级点击 改为相应倍数 结束改为1 :calClickDamage()
 
         // 购买技能时触发
         // 刷新全局DPS倍数
@@ -50,8 +69,8 @@ cc.Class({
             this.calDPSDamage();
             this.calDPSClickDamage();
             this.calClickDamage();
-            this.calClickCrit();
-            this.calClickCritOdds();
+            this.calCritTimes();
+            this.calCritOdds();
         },
         // 计算全局DPS倍数
         calGlobalDPSTimes(){
@@ -61,7 +80,7 @@ cc.Class({
                     times*=hero.getGlobalDPSTimes();
                 }
             });
-            this.globalDPSTimes = times;
+            this.globalDPSTimes = times * this.skDPSTimes;
         },
         // 计算全局金币倍数
         calGoldTimes(){
@@ -71,7 +90,7 @@ cc.Class({
                     times*=hero.getGlobalGoldTimes();
                 }
             });
-            this.globalGoldTimes = times;
+            this.globalGoldTimes = times * this.skGoldTimes;
         },
         // 计算总DPS伤害
         calDPSDamage(){
@@ -97,26 +116,27 @@ cc.Class({
         // 计算总点击伤害
         calClickDamage() {
             var baseClickDamage = new BigNumber(HeroDatas.getHero(0).DPS);
-            this.clickDamage = baseClickDamage.plus(1).plus(this.DPSClickDamage);
+            this.clickDamage = baseClickDamage.plus(1).plus(this.DPSClickDamage).times(this.skClickTimes);
             // this.clickDamage = HeroDatas.getHero(0).DPS + 1 + this.DPSClickDamage;
         },
         // 计算点击暴击倍数
-        calClickCrit(){
-
+        calCritTimes(){
+            this.critTimes = this.cskCritTimes * this.addCritTimes;
         },
         // 计算点击暴击概率
-        calClickCritOdds(){
-
+        calCritOdds(){
+            this.critOdds = this.cskCritOdds + this.skCritOdds;
         },
 
         //====下面是针对古神的====
+        //刷新金身英雄
         refreshGoldenHero(){
             HeroDatas.heroList.forEach(hero => {
                 if (hero.golden > 0) {
                     hero.refresh();
                 }
             });
-            this.calDPSDamage();
+            this.refresh();
         },
     }
 })
--- ClickHeroByCreator/assets/Script/heros/AncientData.js
+++ ClickHeroByCreator/assets/Script/heros/AncientData.js
@@ -79,9 +79,10 @@ cc.Class({
         } else if (this.id == 3) {
             // + 2s Powersurge持续时间
             GameData.addPowersurgeSecond = this.level * 2;
+            GameData.refresh();
         } else if (this.id == 4) {
-            // +15% 暴击伤害
-            GameData.addBjTimes = this.level * 0.15;
+            // +15% 暴击伤害 
+            GameData.addCritTimes = this.level * 0.15;
         } else if (this.id == 5) {
             // 减少boss生命 -5×(1-e^-0.002n) * 10% boss生命
             // 感觉太废物了 所以懒得做
@@ -95,10 +96,74 @@ cc.Class({
             // 英雄费用降低 99.99999999×(1-e^-0.01n)
             GameData.buyHeroDisCount = 99.99999999 * (1 - Math.exp(-0.01*this.level));
         } else if (this.id == 9) {
-            // 	宝箱金币倍数增加
-            
-        } else if (this.id == 2) {
-            
+            // 	宝箱金币倍数增加 9900×(1-e^-0.002n)
+            GameData.addTreasureOdds = 0.01 * (9900 * (1 - Math.exp(-0.02*this.level))/100.0);
+        } else if (this.id == 10) {
+            // 增加金币探测器持续时间2s MetalDetector
+            GameData.addMetalDetectorSecond = this.level * 2;
+        } else if (this.id == 11) {
+            // 普怪 宝箱 10倍金币的概率
+            GameData.addTenfoldGoldOdds = 100 * (1 - Math.exp(-0.0025*this.level));
+        } else if (this.id == 12) {
+            // +20% click damage
+            GameData.addClickDamageTimes = 1 + 0.2 * this.level;
+            GameData.calClickDamage();
+        } else if (this.id == 13) {
+            // 	+2s Super Clicks duration
+            GameData.addSuperClickSecond = this.level * 2;
+        } else if (this.id == 14) {
+            // 附加DPS点击伤害倍数
+            GameData.addDPSClickDamageTimes = 0.0001*this.level;
+            // 计算点击伤害
+        } else if (this.id == 15) {
+            // +2s Golden Clicks duration
+            GameData.addGoldClickSecond = this.level * 2;
+        } else if (this.id == 16) {
+            // 减少每关怪物数量 暂时不要
+        } else if (this.id == 17) {
+            // 加闲置金币
+            let lv = this.level - 99;//纳税等级'
+            let diff = 0;
+            if (lv > 0) {
+                let c = Math.min(Math.ceil(lv / 10),15);
+                for (let i = 0; i < c; i++) {
+                    diff += (lv - i * 10);
+                }
+            }
+            GameData.addLeaveGoldTimes = this.level*0.25 - diff*0.01;
+        } else if (this.id == 18) {
+            // +5% Gold
+            GameData.addGoldTimes = this.level * 0.05;
+            // 
+        } else if (this.id == 19) {
+            // 加宝箱金币
+            GameData.addTreasureTimes = 1 + 0.5 * this.level;
+        } else if (this.id == 20) {
+            // soul dps 不做
+        } else if (this.id == 21) {
+            // 	空闲时每个未分配的自动点击器+ 10％Gold（没有点击60秒）。 放弃。。
+        } else if (this.id == 22) {
+            // +30% gold from Golden Clicks 
+            GameData.addGoldClickTimes = 1 + 0.3*this.level;
+        } else if (this.id == 23) {
+            // 红宝石掉落后可点击双重红宝石的机会增加
+        } else if (this.id == 24) {
+            // 加闲置DPS伤害
+            let lv = this.level - 99;//纳税等级'
+            let diff = 0;
+            if (lv > 0) {
+                let c = Math.min(Math.ceil(lv / 10),15);
+                for (let i = 0; i < c; i++) {
+                    diff += (lv - i * 10);
+                }
+            }
+            GameData.addLeaveDPSTimes = this.level*0.25 - diff*0.01;
+        } else if (this.id == 25) {
+            // 增加暴击风暴时间 +2s
+            GameData.addCritStormSecond = this.level * 2;
+        } else if (this.id == 26) {
+            // 技能冷却减少 75×(1-e-0.026n)
+            GameData.addSkillCoolReduction = 0.75 * (1 - Math.exp(-0.026*this.level));
         }
     },
 
