a1ee8ad6d6c1d8b0d5056f5d075d593756b5e164
CoreXJ <17721028010@163.com>
2018-12-01 13:32:32 +0800

    先提交下

--- ClickHeroByCreator/assets/Script/formulas/GameData.js
+++ ClickHeroByCreator/assets/Script/formulas/GameData.js
@@ -25,27 +25,30 @@ cc.Class({
 
         playerStatus : 0,// 玩家状态 0:蒙蔽 1:闲置 2:combo
 
+        heroLvUnit: 1, // 
+        ancientLvUnit: 1,
+
         //--------古神的影响--------
         //说明：//[id][* 家恒支持][- 监听ON_UPGRADE_ANCIENT改变UI]
-        addGoldenDpsTimes : 0,      //1- 所有金身加成倍数2% 0.02++
-        addPrimalBossOdds : 0,      //2* 增加远古Boss出现几率 0~0.75
+        addGoldenDpsTimes : 0,      //1- 所有金身加成倍数2% 0.02++ √
+        addPrimalBossOdds : 0,      //2* 增加远古Boss出现几率 0~0.75 ?
         addPowersurgeSecond: 0,    //3*- Powersurge秒数增加 2s++ √
-        addCritTimes : 1,           //4 古神附加暴击倍数
+        addCritTimes : 1,           //4 古神附加暴击倍数 √
         addClickstormSecond: 0,    //6*- 点击风暴秒数增加 2s++ √
         addBossTimerSecond: 0,     //7* Boss计时器持续时间 0~30.0s √
-        buyHeroDiscount : 1,        //8- 购买英雄折扣 0~1
+        buyHeroDiscount : 1,        //8- 购买英雄折扣 0~1 √
         addTreasureOdds: 0.01,     //9* 宝箱出现概率 0~1 √
         addMetalDetectorSecond: 0, //10*- 金币探测器 2s++ √
         addTenfoldGoldOdds : 0,     //11* 普怪 宝箱 10倍金币的概率 0~1 √
-        addClickDamageTimes : 1,    //12- 点击伤害倍数 每级+20%
+        addClickDamageTimes : 1,    //12- 点击伤害倍数 每级+20% √
         addSuperClickSecond: 0,    //13*- 超级点击时间 2s++ √
-        addDPSClickDamageTimes : 0, //14- 附加DPS点击伤害倍数
+        addDPSClickDamageTimes : 0, //14- 附加DPS点击伤害倍数 √
         addGoldClickSecond: 0,     //15*- 点金手时间 2s++ √
-        addLeaveGoldTimes : 1,      //17- 闲置金币倍数
+        addLeaveGoldTimes : 1,      //17- 闲置金币倍数 ?
         addGoldTimes: 1,           //18* +5% Gold √
         addTreasureTimes: 1,       //19* 宝箱金币倍数 √
         addGoldClickTimes: 1,      //22*- 点金手倍数 +30% gold from Golden Clicks √
-        addLeaveDPSTimes : 1,       //24- 加闲置DPS伤害
+        addLeaveDPSTimes : 1,       //24- 加闲置DPS伤害 ?
         addCritStormSecond: 0,     //25*- 增加暴击风暴时间 +2s √
         addSkillCoolReduction: 0,  //26*- 技能冷却减少 0~1 √
 
@@ -122,7 +125,8 @@ cc.Class({
         // 计算总点击伤害
         calClickDamage() {
             var baseClickDamage = new BigNumber(HeroDatas.getHero(0).DPS);
-            this.clickDamage = baseClickDamage.plus(1).plus(this.DPSClickDamage).times(this.skClickTimes);
+            this.clickDamage = baseClickDamage.plus(1).plus(this.DPSClickDamage)
+                .times(this.skClickTimes).times(this.addClickDamageTimes);
             // this.clickDamage = HeroDatas.getHero(0).DPS + 1 + this.DPSClickDamage;
         },
         // 计算点击暴击倍数
