a66b871aa7e530ddbc5580044102e9cfd5d054c5
CoreXJ <17721028010@163.com>
2019-06-05 18:09:00 +0800

    紧急提交点小bug

--- ClickHeroByCreator/assets/Script/MySV.js
+++ ClickHeroByCreator/assets/Script/MySV.js
@@ -33,43 +33,47 @@ cc.Class({
             this._b = 0
             return
         }
-        let h = this.scrollView.node.height
-        let tv = childs[this._t]
-        let ty = this.getPositionInView(tv);
-        
-        if (this._b > 3 && ty > h / 2 + 140) {
-            tv.active = false
-            // console.log("del t "+this._t);
-            tv = null
-            this._t ++
-        }
-        let bv = childs[this._b]
-        let by = this.getPositionInView(bv);
-        if (this._b > 3 && by < -h / 2 - 140) {
-            bv.active = false
-            // console.log("del b "+this._b);
-            bv = null
-            this._b --
-        }
-        if (tv && this._t > 0) {
-            let ti = this._t - 1
-            tv = childs[ti]
-            ty = this.getPositionInView(tv);
-            if (ty < h / 2 + 140) {
-                tv.active = true
-                this._t = ti
-                // console.log("add t "+this._t);
+        try {
+            let h = this.scrollView.node.height
+            let tv = childs[this._t]
+            let ty = this.getPositionInView(tv);
+            
+            if (this._b > 3 && ty > h / 2 + 140) {
+                tv.active = false
+                // console.log("del t "+this._t);
+                tv = null
+                this._t ++
             }
-        }
-        if (bv && this._b < childs.length-1) {
-            let bi = this._b + 1
-            bv = childs[bi]
-            by = this.getPositionInView(bv);
-            if (by > -h / 2 - 140) {
-                bv.active = true
-                this._b = bi
-                // console.log("add b "+this._b);
+            let bv = childs[this._b]
+            let by = this.getPositionInView(bv);
+            if (this._b > 3 && by < -h / 2 - 140) {
+                bv.active = false
+                // console.log("del b "+this._b);
+                bv = null
+                this._b --
             }
+            if (tv && this._t > 0) {
+                let ti = this._t - 1
+                tv = childs[ti]
+                ty = this.getPositionInView(tv);
+                if (ty < h / 2 + 140) {
+                    tv.active = true
+                    this._t = ti
+                    // console.log("add t "+this._t);
+                }
+            }
+            if (bv && this._b < childs.length-1) {
+                let bi = this._b + 1
+                bv = childs[bi]
+                by = this.getPositionInView(bv);
+                if (by > -h / 2 - 140) {
+                    bv.active = true
+                    this._b = bi
+                    // console.log("add b "+this._b);
+                }
+            }
+        } catch (error) {
+            
         }
     },
     // 计算View在ScrollView中的位置
