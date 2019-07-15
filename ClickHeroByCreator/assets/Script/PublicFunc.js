06b2b558c7099465393d3bb676c161aa742bc818
CoreXJ <corexjay@gmail.com>
2019-07-15 18:32:58 +0800

    补提交

--- ClickHeroByCreator/assets/Script/PublicFunc.js
+++ ClickHeroByCreator/assets/Script/PublicFunc.js
@@ -218,9 +218,9 @@ cc.Class({
     },
 
     showUpgradeInfo(){
-        const info = ["1.增加新神器照妖镜",
-        "2.增加新神器玉净瓶，以及它的加持",
-        "3.古神增加手动输入升级等级",
+        const info = ["1.游戏中每隔一会儿会出现一个冰糕，随机获得妖丹、仙丹、仙桃",
+        "2.增加新神器照妖镜",
+        "3.增加新神器玉净瓶",
             ].join("\n")
         this.popDialog({
             contentStr: info,
@@ -266,8 +266,8 @@ cc.Class({
 
     makeNextClickruby(){
         // 随机5~10分钟产生一个nextClickruby
-        // let nextTime = Date.now() + (Math.random()+1) * 5 * 1000 * 60
-        let nextTime = Date.now() + 1000 * 10
+        let nextTime = Date.now() + (Math.random()+1) * 5 * 1000 * 60
+        // let nextTime = Date.now() + 1000 * 10
         let r = Math.random()
         let type
         if (r <= 0.5) {
@@ -302,7 +302,7 @@ cc.Class({
                 soul = soul.plus(Formulas.getPrimalBossSoul(lv))
             }
         }
-        soul = soul.times(GameData.getPrimalBossOdds() + 0.25).plus(4).integerValue()
+        soul = soul.times(GameData.getPrimalBossOdds()).plus(4).integerValue()
         return soul
     },
 });
